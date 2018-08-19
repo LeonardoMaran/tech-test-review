43af50f2c3574db3cace2fdec402198a1256fb79
anguspettifer <angus.pettifer@gmail.com>
2018-08-19 18:56:56 +0100

    Add tests for ArticleList Controller

--- frontend/public/javascripts/controllers/ArticleListController.js
+++ frontend/public/javascripts/controllers/ArticleListController.js
@@ -6,12 +6,14 @@ class ArticleListController {
     }
   }
 
-  render(){
+  render(document = document){
     this.state.articleList.createList();
     let listHTML = this.state.articleListView.format(this.state.articleList.viewList())
     let title = document.getElementById('title');
     title.innerHTML = listHTML
   }
+
+  inputHTML(){}
 }
 
 module.exports = ArticleListController;
---
+++ frontend/public/tests/ArticleList-controller.test.js
@@ -0,0 +1,57 @@
+const ArticleListController = require('../javascripts/controllers/ArticleListController');
+
+// clearly all these mocks need to be refactored into the mock file.
+
+class ArticleListMock {
+  constructor() {
+    this.createListCalled = 0
+  }
+
+  createList(){
+    this.createListCalled += 1
+  }
+
+  viewList(){}
+}
+
+class ArticleListViewMock {
+  constructor() {
+    this.formatCalled = 0
+  }
+
+  format(){
+    this.formatCalled += 1
+  }
+}
+
+class ElementMock {
+  innerHTML(){}
+}
+
+class DocumentMock {
+  getElementById(){
+    return new ElementMock
+  }
+
+}
+
+const document = new DocumentMock
+
+const data = {}
+
+describe('ArticleListController', () => {
+
+  let articleListController = new ArticleListController(data, ArticleListMock, ArticleListViewMock)
+
+//I want to have some sort of beforeEach here
+  articleListController.render(document);
+
+  it('creates a new list', () => {
+    expect(articleListController.state.articleList.createListCalled).toEqual(1)
+  })
+
+  it('formats the list', () => {
+    expect(articleListController.state.articleListView.formatCalled).toEqual(1)
+  })
+
+});
