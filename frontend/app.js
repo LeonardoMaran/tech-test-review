0c6f04d134097040eb0ebcd26443b551b5b4495f
anguspettifer <angus.pettifer@gmail.com>
2018-08-19 17:48:29 +0100

    Set up express for front end

---
+++ frontend/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Set Static Path
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(3000, function(){
+  console.log('server started on port 3000...');
+})
