d3022631ee81e55007fa77f26f7bcb4a6d3e8e43
anguspettifer <angus.pettifer@gmail.com>
2018-08-20 07:47:37 +0100

    Set up API call to FT API
    * Post request from back end using requset
    * Get request from front end for ease of set up

--- backend/server/routes/index.js
+++ backend/server/routes/index.js
@@ -1,9 +1,31 @@
 module.exports = (app) => {
-  
+
   app.get('/api', (req, res) => {
     return res.status(200).send({
       message: 'Welcome to the Goose FT API!',
     })
   });
 
+  app.get('/articles', (req, res) => {
+    var request = require("request");
+
+    var options = { method: 'POST',
+      url: 'http://api.ft.com/content/search/v1',
+      headers:
+       { 'Cache-Control': 'no-cache',
+         'Content-Type': 'application/json',
+         'X-Api-Key': '59cbaf20e3e06d3565778e7b222bef626a1744baa0d6e318798d127e' },
+      body:
+       { queryString: 'sections:"Energy"',
+         resultContext: { aspects: [ 'title', 'lifecycle', 'location', 'summary', 'editorial' ] } },
+      json: true };
+
+      var util = require('util')
+
+    request(options, function (error, response, body) {
+      if (error) throw new Error(error);
+      res.status(200).send(body);
+    })
+  });
+  
 };
--- frontend/public/javascripts/application.js
+++ frontend/public/javascripts/application.js
@@ -13,4 +13,17 @@ document.addEventListener('DOMContentLoaded', function() {
     })
     .catch(err => console.log(err))
 
+  let articles = 'http://localhost:3010/articles';
+
+  fetch(articles)
+  .then(response => {
+    if (response.status !== 200) {
+      console.log("We have a problem with articles: " + response.status);
+      return;
+    }
+    response.json()
+    .then(data => {console.log(data);});
+  })
+  .catch(err => console.log(err))
+
 });
