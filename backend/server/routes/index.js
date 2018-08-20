6568556faa1c8bc10658aab1667c3339334641b4
anguspettifer <angus.pettifer@gmail.com>
2018-08-20 21:38:28 +0100

    Refactor back end index.js

--- backend/server/routes/index.js
+++ backend/server/routes/index.js
@@ -1,15 +1,9 @@
 module.exports = (app) => {
 
-  app.get('/api', (req, res) => {
-    return res.status(200).send({
-      message: 'Welcome to the Goose FT API!',
-    })
-  });
-
   app.get('/articles', (req, res) => {
-    var request = require("request");
+    const request = require("request");
 
-    var options = { method: 'POST',
+    const options = { method: 'POST',
       url: 'http://api.ft.com/content/search/v1',
       headers:
        { 'Cache-Control': 'no-cache',
@@ -20,12 +14,36 @@ module.exports = (app) => {
          resultContext: { aspects: [ 'title', 'lifecycle', 'location', 'summary', 'editorial' ] } },
       json: true };
 
-      var util = require('util')
-
     request(options, function (error, response, body) {
       if (error) throw new Error(error);
       res.status(200).send(body);
     })
   });
-  
+
+  app.post('/searchrequest', (req, res) => {
+
+    const request = require("request");
+
+    const sections = 'sections:' + req.body.search
+
+    const options = { method: 'POST',
+      url: 'http://api.ft.com/content/search/v1',
+      headers:
+       { 'Cache-Control': 'no-cache',
+         'Content-Type': 'application/json',
+         'X-Api-Key': '59cbaf20e3e06d3565778e7b222bef626a1744baa0d6e318798d127e' },
+      body:
+       { queryString: {},
+         resultContext: { aspects: [ 'title', 'lifecycle', 'location', 'summary', 'editorial' ] } },
+      json: true };
+
+      options.body.queryString = sections
+
+    request(options, function (error, response, body) {
+      if (error) throw new Error(error);
+      res.status(200).send(body);
+    })
+
+  })
+
 };
