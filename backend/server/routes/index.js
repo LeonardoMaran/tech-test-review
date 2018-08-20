0c11be0c60a4fd1112ca0c657e2aa574fdd2dc53
anguspettifer <angus.pettifer@gmail.com>
2018-08-20 22:04:33 +0100

    refactor backend routing

--- backend/server/routes/index.js
+++ backend/server/routes/index.js
@@ -1,19 +1,19 @@
 module.exports = (app) => {
 
+  const request = require("request");
+
+  const options = { method: 'POST',
+    url: 'http://api.ft.com/content/search/v1',
+    headers:
+     { 'Cache-Control': 'no-cache',
+       'Content-Type': 'application/json',
+       'X-Api-Key': '59cbaf20e3e06d3565778e7b222bef626a1744baa0d6e318798d127e' },
+    body:
+     { queryString: 'sections:"Energy"',
+       resultContext: { aspects: [ 'title', 'lifecycle', 'location', 'summary', 'editorial' ] } },
+    json: true };
+
   app.get('/articles', (req, res) => {
-    const request = require("request");
-
-    const options = { method: 'POST',
-      url: 'http://api.ft.com/content/search/v1',
-      headers:
-       { 'Cache-Control': 'no-cache',
-         'Content-Type': 'application/json',
-         'X-Api-Key': '59cbaf20e3e06d3565778e7b222bef626a1744baa0d6e318798d127e' },
-      body:
-       { queryString: 'sections:"Energy"',
-         resultContext: { aspects: [ 'title', 'lifecycle', 'location', 'summary', 'editorial' ] } },
-      json: true };
-
     request(options, function (error, response, body) {
       if (error) throw new Error(error);
       res.status(200).send(body);
@@ -21,24 +21,8 @@ module.exports = (app) => {
   });
 
   app.post('/searchrequest', (req, res) => {
-
-    const request = require("request");
-
     const sections = 'sections:' + req.body.search
-
-    const options = { method: 'POST',
-      url: 'http://api.ft.com/content/search/v1',
-      headers:
-       { 'Cache-Control': 'no-cache',
-         'Content-Type': 'application/json',
-         'X-Api-Key': '59cbaf20e3e06d3565778e7b222bef626a1744baa0d6e318798d127e' },
-      body:
-       { queryString: {},
-         resultContext: { aspects: [ 'title', 'lifecycle', 'location', 'summary', 'editorial' ] } },
-      json: true };
-
-      options.body.queryString = sections
-
+    options.body.queryString = sections
     request(options, function (error, response, body) {
       if (error) throw new Error(error);
       res.status(200).send(body);
